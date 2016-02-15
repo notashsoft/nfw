e58af2b880f7bed7b925f15d203db8116ae3bc30
mirzaaghazadeh <mirzaaghazadeh.n@gmail.com>
2016-02-15 23:38:01 +0330

    Create gravatar.php
    Get Gravatars image url

---
+++ core/plugin/gravatar.php
@@ -0,0 +1,24 @@
+<?php
+class gravatar{
+
+    // Navid Mirzaahazadeh
+    // 2016/03/15
+    // Navidm.ir
+    // inavid.ir@gmail.com
+
+	function get( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+    $url = 'http://www.gravatar.com/avatar/';
+    $url .= md5( strtolower( trim( $email ) ) );
+    $url .= "?s=$s&d=$d&r=$r";
+    if ( $img ) {
+        $url = '<img src="' . $url . '"';
+        foreach ( $atts as $key => $val )
+            $url .= ' ' . $key . '="' . $val . '"';
+        $url .= ' />';
+    }
+    return $url;
+}
+
+
+}
+?>
